988db7e72e4107f6f112baf4f8092f2d238c031d
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2025-01-01 22:08:12 +0100

    Fix return type error in login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -20,7 +20,7 @@ import { useRouter } from "next/navigation";
 export default function Page({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+}: any) {
   const router = useRouter();
   const { register, handleSubmit } = useForm<LoginData>({
     resolver: zodResolver(loginValidationSchema),
